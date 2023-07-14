73eaef75cbccd5b44f04dfa4e0cf60ebac89f638
Tiago Angelo <tiagoangelods.dev@gmail.com>
2023-07-14 15:27:28 -0300

    Added Switch componetn

--- .storybook/storybook.requires.js
+++ .storybook/storybook.requires.js
@@ -55,7 +55,7 @@ const getStories = () => {
     "./src/components/color.red.stories.ts": require("../src/components/color.red.stories.ts"),
     "./src/components/color.secondary-gray.stories.ts": require("../src/components/color.secondary-gray.stories.ts"),
     "./src/components/color.yellow.stories.ts": require("../src/components/color.yellow.stories.ts"),
-    "./src/components/Input/index.stories.ts": require("../src/components/Input/index.stories.ts"),
+    "./src/components/Switch/index.stories.ts": require("../src/components/Switch/index.stories.ts"),
     "./src/components/Text/index.stories.ts": require("../src/components/Text/index.stories.ts"),
   };
 };
